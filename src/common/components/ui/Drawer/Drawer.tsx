c6ce9627ec5779ed1aed438d26636cf34b7b6428
NastiaCu <nastiacu24@gmail.com>
2022-10-22 21:05:35 +0300

    improved Navbar

---
+++ src/common/components/ui/Drawer/Drawer.tsx
@@ -0,0 +1,92 @@
+import React, { FC, useState } from 'react'
+import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Typography, ListItem, Box, Tabs, Tab } from '@mui/material'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { Link } from "react-router-dom";
+
+type PageLinkProps = {
+    to: string;
+    title: string;
+};
+
+const PageLink: FC<PageLinkProps> = ({ to, title }) => {
+    return (
+        <Link to={to} style={{ textDecoration: "none" }}>
+            <Typography variant="h6" color="#fff">
+                {title}
+            </Typography>
+        </Link>
+    );
+};
+
+const DrawerComp = () => {
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            <Drawer PaperProps={{
+                sx: { width: "40%", backgroundColor: "#222B42", borderRadius: "0 2rem 2rem 0" }
+            }}
+                open={open} onClose={() => setOpen(false)}>
+                <List>
+                    <ListItemButton onClick={() => setOpen(false)} >
+                        <ListItemIcon >
+                            <ListItemText>
+                                <ListItem>
+                                    <Typography style={{ fontWeight: 600 }} variant="h5" color="primary.contrastText" >Customers++</Typography>
+                                </ListItem>
+
+                                <ListItem button sx={{
+                                    borderRadius: "2rem",
+                                    '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}>
+                                    <PageLink to="/" title="Home" />
+                                </ListItem>
+
+                                <ListItem button sx={{
+                                    borderRadius: "2rem",
+                                    '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}>
+                                    <PageLink to="/metrics" title="Metrics" />
+                                </ListItem>
+
+                                <ListItem button sx={{
+                                    borderRadius: "2rem",
+                                    '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}>
+                                    <PageLink to="/statistics" title="Statistics" />
+                                </ListItem>
+                                <ListItem button sx={{
+                                    borderRadius: "2rem",
+                                    '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}>
+                                    <PageLink to="/login" title="Login" />
+                                </ListItem>
+                            </ListItemText>
+                        </ListItemIcon>
+                    </ListItemButton>
+                </List>
+            </Drawer>
+            <IconButton onClick={() => setOpen(!open)}>
+                <MenuRoundedIcon sx={{
+                    width: "3rem", height: "3rem", color: "primary.contrastText", '&:hover': {
+                        backgroundColor: 'primary.main',
+                        opacity: [0.9, 0.8, 0.7],
+                    },
+                }} />
+            </IconButton>
+        </>
+    )
+}
+
+export default DrawerComp
--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-import { Box, Container, Divider, Paper, Theme, Typography } from "@mui/material";
-import { FC } from "react";
+import { Box, Container, Divider, Paper, Theme, Typography, SxProps, useMediaQuery, Toolbar, Button, Tab, styled } from "@mui/material";
+import { FC, ReactNode, useState } from "react";
 import { Settings as SettingsIcon } from "@mui/icons-material";
 import { colors } from "@common/theme/utils/consts";
+import DrawerComp from "@common/components/ui/Drawer/Drawer";
+
 
 type PageLinkProps = {
   to: string;
   title: string;
 };
+
 const PageLink: FC<PageLinkProps> = ({ to, title }) => {
   return (
     <Link to={to} style={{ textDecoration: "none" }}>
@@ -18,33 +21,93 @@ const PageLink: FC<PageLinkProps> = ({ to, title }) => {
   );
 };
 
+const BootstrapButton = styled(Button)({
+  borderRadius: "2rem",
+  '&:hover': {
+    backgroundColor: 'primary.main',
+    opacity: [0.9, 0.8, 0.7],
+  },
+});
+
 const Navbar: FC = () => {
+  const isMatch = useMediaQuery("(max-width:600px)");
+
   return (
+
     <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          height: "53px",
-          gap: "1rem",
-          p: "0 1rem",
-          overflow: "hidden",
-          zIndex: (theme: Theme) => theme.zIndex.drawer + 1,
-          bgcolor: colors.BACKGROUND_MAIN,
-        }}
-      >
-        <PageLink to="/" title="Home" />
-        <PageLink to="/metrics" title="Metrics" />
-        <PageLink to="/statistics" title="Statistics" />
-        <PageLink to="/login" title="Login" />
+      <Toolbar> {isMatch ? <>
+        <DrawerComp />
         <Box sx={{ ml: "auto" }}>
           <Link to="/settings">
             <SettingsIcon sx={{ width: "3rem", height: "3rem", color: "primary.contrastText" }} />
           </Link>
         </Box>
-      </Box>
+      </> :
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "65px",
+            gap: "1.5rem",
+            p: "0 1rem",
+            overflow: "hidden",
+            zIndex: (theme: Theme) => theme.zIndex.drawer + 1,
+            bgcolor: colors.BACKGROUND_MAIN,
+          }
+          }
+        >
+          <> <Box
+            component="img"
+            sx={{ width: "3rem", height: "3rem" }}
+            alt="logo"
+            src={require("/assets/images/logo.png")}
+          />
+            <BootstrapButton variant="contained" disableRipple>
+              <PageLink to="/" title="Home" />
+            </BootstrapButton>
+
+            <Button sx={{
+              borderRadius: "2rem",
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}>
+              <PageLink to="/metrics" title="Metrics" />
+            </Button>
+
+            <Button sx={{
+              borderRadius: "2rem",
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}>
+              <PageLink to="/statistics" title="Statistics" />
+            </Button>
+
+            <Button sx={{
+              borderRadius: "2rem",
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}>
+              <PageLink to="/login" title="Login" />
+            </Button>
+
+            <Box sx={{ ml: "auto" }}>
+              <Link to="/settings">
+                <SettingsIcon sx={{ width: "3rem", height: "3rem", color: "primary.contrastText" }} />
+              </Link>
+            </Box>
+          </>
+        </Box>
+      }
+      </Toolbar>
+
       <Divider sx={{ bgcolor: "primary.light", m: "0 5px" }} />
     </>
   );
